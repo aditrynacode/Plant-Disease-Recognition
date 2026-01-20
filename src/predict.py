ab7400f863010c9a8359c7b9ebd22da10b8c31f9
aditrynacode <adityabsingh010106@gmail.com>
2026-01-21 02:10:59 +0530

    Added prediction script

---
+++ src/predict.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+model = tf.keras.models.load_model("model/pdr_model.keras")
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    "data/test",
+    image_size = (256, 256),
+    batch_size = 32,
+    shuffle = False
+)
+
+class_names = test_ds.class_names
+
+normalizer = tf.keras.layers.Rescaling(1/255)
+test_ds = test_ds.map(lambda x, y: (normalizer(x), y))
+
+image = Image.open("data/test/healthy/8e858c8397706b7b.jpg")
+
+img = tf.keras.utils.load_img("data/test/healthy/8e858c8397706b7b.jpg", target_size=(256, 256))
+img = tf.keras.utils.img_to_array(img)   
+img = img / 255          
+img = tf.expand_dims(img, axis=0)      
+
+prediction = model.predict(img)
+predicted_number = np.argmax(prediction)
+predicted_label = class_names[predicted_number]
+
+plt.imshow(image)
+plt.title(f"Predicted: {predicted_label}")
+plt.axis("off")
+plt.show() 
